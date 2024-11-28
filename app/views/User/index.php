186b03284d5e3c1f771110368a846fe7b0c3d3f8
IsDukeThere <masduqialfiyan.acc@gmail.com>
2024-11-29 02:31:52 +0700

    update content

--- app/views/User/index.php
+++ app/views/User/index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="<?= img ?>img/2.png" type="image/x-icon">
     <title>Informasi Bencana Longsor | Beranda</title>
 </head>
 <style>
@@ -17,12 +18,73 @@
         background-color: #72604B;
     }
 
-    .background {
+    #scroll-btn {
+        display: none;
+        position: fixed;
+        bottom: 20px;
+        right: 30px;
+        z-index: 99;
+        font-size: 18px;
+        border: none;
+        outline: none;
+        background-color: #F2CCA0;
+        color: #6A5946;
+        cursor: pointer;
+        padding: 10px 19px;
+        border-radius: 120px;
+    }
+
+    #scroll-btn:hover {
+        background-color: #6A5946;
+        color: #F2CCA0;
+    }
+
+    /* .background {
         margin: 20px 25px 20px 25px;
         padding-bottom: 60px;
         border-radius: 10px;
         background-image: url('<?= img ?>img/pexels-zac-frith-325758-918794.jpg');
         background-size: cover;
+        filter: brightness(1) grayscale(5) sepia(0.3);
+    } */
+
+    /* background-image {
+        filter: brightness(10%);
+        filter: grayscale(25%);
+        filter: sepia(80%);
+    } */
+
+    .background {
+        margin: 20px 25px;
+        padding-bottom: 60px;
+        border-radius: 10px;
+        position: relative;
+        /* Penting untuk pseudo-elemen */
+        overflow: hidden;
+        /* Menghindari pseudo-elemen keluar dari batas elemen */
+    }
+
+    .background::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        border-radius: 10px;
+        background-image: url('<?= img ?>img/pexels-zac-frith-325758-918794.jpg');
+        background-size: cover;
+        background-position: center;
+        filter: brightness(0.6) grayscale(5) sepia(0.6);
+        /* Filter hanya diterapkan di pseudo-elemen */
+        z-index: 0;
+        /* Pastikan lapisan ini di bawah konten */
+    }
+
+    .background>* {
+        position: relative;
+        z-index: 1;
+        /* Konten di atas pseudo-elemen */
     }
 
     .navbar {
@@ -51,6 +113,7 @@
     }
 
     .about a {
+        margin-top: 50px;
         font-family: 'Montserrat';
         font-weight: 650;
         font-size: 16px;
@@ -60,20 +123,12 @@
     .landing {
         display: flex;
         flex-direction: column;
-
-    }
-
-    .landing p {
-        font-family: 'Montserrat';
-        font-weight: 700;
-        font-size: 150px;
-        color: white;
-        margin-left: 40px;
-
+        margin-top: 50px;
     }
 
     .lower {
-        margin-top: 40px;
+        padding-top: 40px;
+        padding-bottom: 30px;
     }
 
     .short-link {
@@ -125,11 +180,15 @@
         background-size: cover;
         color: #F2CCA0;
         text-align: justify;
+        display: grid;
+        grid-template-columns: repeat(2, auto);
+        grid-template-rows: repeat(3, auto);
     }
 
     .jenis h2 {
         font-weight: 800;
-        padding-bottom: 15px;
+        padding-top: 20px;
+        padding-bottom: 30px;
     }
 
     .jenis h4 {
@@ -232,27 +291,55 @@
     .mitigasi {
         margin: 20px 20px 20px 20px;
         padding: 20px 20px 20px 20px;
+        text-align: justify;
     }
 
     .mitigasi h2 {
         font-weight: 800;
         padding-bottom: 15px;
     }
+
+    .mitigasi p {
+        padding-left: 50px;
+        padding-right: 60px;
+        font-size: 18px;
+    }
 </style>
 
 <body>
+    <button onclick="topFunction()" id="scroll-btn" title="Top">&uarr;</button>
+    <script>
+        // ketika pengunjung scroll kebawah 20px dari atas dokumen, maka tampilkan tombol scroll-btn
+        window.onscroll = function() {
+            scrollFunction()
+        };
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                document.getElementById("scroll-btn").style.display = "block";
+            } else {
+                document.getElementById("scroll-btn").style.display = "none";
+            }
+        }
+
+        // ketika tombol tersebut di klik, maka lakukan scroll keatas laman
+        function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+    </script>
     <!-- ini cuma bagian navbar, tulisan gede Tanah Longsor sama  -->
     <div class="background">
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
                 <div class="brand">
-                    <a class="navbrand">Anggep Aja Logo</a>
+                    <a href="#" class="navbrand"><img src="<?= img ?>img/1.png" alt="SI Longsor" width="120px"></a>
                 </div>
                 <div class="collapse navbar-collapse justify-content-center" id="NavbarMenu">
                     <div class="nav-item position-relative" id="navspymenu">
                         <a href="#scrollspyPeta">Peta Bencana</a>
                         <a href="#scrollspyMitigasi">Mitigasi</a>
-                        <a href="#scrollspyPenanganan">Penanganan</a>
+                        <a href="#scrollspyPenanganan">Penanggulangan</a>
                     </div>
                 </div>
                 <div class="about">
@@ -262,8 +349,8 @@
         </nav>
 
         <div class="landing">
-            <p>Tanah</p>
-            <p>Longsor</p>
+            <p style="font-family: 'Montserrat'; font-weight: 600; font-size: 60px; color: white; margin-left: 40px;">Sistem Informasi</p>
+            <p style="font-family: 'Montserrat'; font-weight: 700; font-size: 140px; color: white; margin-left: 40px;">Tanah Longsor</p>
         </div>
         <div class="lower">
             <div class="short-link" id="navspy">
@@ -273,6 +360,7 @@
             </div>
             <div class="short-link" id="navspy">
                 <a href="#scrollspydampak">Dampak</a>
+                <a href="#scrollspylapor">Lapor Longsor</a>
             </div>
         </div>
     </div>
@@ -301,71 +389,84 @@
     <div class="Def-longsor" data-bs-spy="scroll" data-bs-target="#navspy">
         <div class="jenis" id="scrollspyJenis">
             <h2>Jenis - Jenis Tanah Longsor</h2>
-            <h4>Longsoran Geser (Translasi)</h4>
+            <h2> </h2>
             <div class="image">
                 <img src="<?= img ?>img/longsorantranslasi.png" alt="Longsor Translasi" class="image">
             </div>
-            <p>Jenis longsor ini terjadi ketika
-                tanah atau batuan bergerak menuruni
-                lereng secara lurus. Bayangkan seperti
-                papan seluncur yang meluncur di atas
-                permukaan licin. Longsoran ini sering
-                terjadi pada lereng yang bidangnya cukup
-                rata dan lapisan tanahnya tidak terlalu dalam.</p>
+            <div class="content">
+                <h4>Longsoran Geser (Translasi)</h4>
+                <p>Jenis longsor ini terjadi ketika
+                    tanah atau batuan bergerak menuruni
+                    lereng secara lurus. Bayangkan seperti
+                    papan seluncur yang meluncur di atas
+                    permukaan licin. Longsoran ini sering
+                    terjadi pada lereng yang bidangnya cukup
+                    rata dan lapisan tanahnya tidak terlalu dalam.</p>
+            </div>
 
-            <h4>Longsoran Melingkar (Rotasi)</h4>
             <div class="image">
                 <img src="<?= img ?>img/longsoranrotasi.png" alt="Longsor Rotasi" class="image">
             </div>
-            <p>Pada jenis ini, tanah bergerak
-                melengkung atau berputar saat jatuh.
-                Biasanya longsoran ini terjadi pada
-                lereng yang lebih dalam dengan massa
-                tanah yang besar. Gambarnya seperti
-                tanah yang 'menggulung' saat bergerak ke bawah.</p>
+            <div class="content">
+                <h4>Longsoran Melingkar (Rotasi)</h4>
+                <p>Pada jenis ini, tanah bergerak
+                    melengkung atau berputar saat jatuh.
+                    Biasanya longsoran ini terjadi pada
+                    lereng yang lebih dalam dengan massa
+                    tanah yang besar. Gambarnya seperti
+                    tanah yang 'menggulung' saat bergerak ke bawah.</p>
+            </div>
 
-            <h4>Rayapan Tanah (Creep)</h4>
             <div class="image">
                 <img src="<?= img ?>img/rayapantanah.png" alt="Rayapan Tanah" class="image">
             </div>
-            <p>Longsor jenis ini sangat lambat dan tidak
-                langsung terlihat. Tanahnya pelan-pelan
-                sekali bergerak turun dari lereng. Jenis
-                tanahnya berupa butiran kasar dan halus.
-                Meskipun lambat, jenis longsor ini tetap
-                berbahaya karena bisa merusak bangunan
-                atau infrastruktur di sekitarnya dalam jangka panjang.</p>
+            <div class="content">
+                <h4>Rayapan Tanah (Creep)</h4>
+                <p>Longsor jenis ini sangat lambat dan tidak
+                    langsung terlihat. Tanahnya pelan-pelan
+                    sekali bergerak turun dari lereng. Jenis
+                    tanahnya berupa butiran kasar dan halus.
+                    Meskipun lambat, jenis longsor ini tetap
+                    berbahaya karena bisa merusak bangunan
+                    atau infrastruktur di sekitarnya dalam jangka panjang.</p>
+            </div>
 
-            <h4>Runtuhan Batu (Rockfall)</h4>
             <div class="image">
                 <img src="<?= img ?>img/runtuhanbatu.png" alt="Runtuhan Batu" class="image">
             </div>
-            <p>Ini jenis longsor di mana batuan jatuh
-                bebas dari tebing atau lereng yang curam.
-                Biasanya, batu-batu besar yang jatuh, dan
-                bisa sangat berbahaya karena pergerakannya
-                cepat dan tanpa peringatan.</p>
+            <div class="content">
+                <h4>Runtuhan Batu (Rockfall)</h4>
+                <p>Ini jenis longsor di mana batuan jatuh
+                    bebas dari tebing atau lereng yang curam.
+                    Biasanya, batu-batu besar yang jatuh, dan
+                    bisa sangat berbahaya karena pergerakannya
+                    cepat dan tanpa peringatan.</p>
+            </div>
 
-            <h4>pergerakan blok (block slide)</h4>
             <div class="image">
                 <img src="<?= img ?>img/pergerakanblok.png" alt="Pergerakan Blok" class="image">
             </div>
-            <p>Jenis tanah longsor di mana sebuah massa
-                besar dari batu atau tanah bergerak turun
-                sebagai satu kesatuan utuh. Jenis longsor
-                ini biasanya terjadi di lereng yang terbuat
-                dari material yang keras, seperti batu, yang
-                memungkinkan seluruh blok tetap utuh saat bergerak.</p>
+            <div class="content">
+                <h4>pergerakan blok (block slide)</h4>
+                <p>Jenis tanah longsor di mana sebuah massa
+                    besar dari batu atau tanah bergerak turun
+                    sebagai satu kesatuan utuh. Jenis longsor
+                    ini biasanya terjadi di lereng yang terbuat
+                    dari material yang keras, seperti batu, yang
+                    memungkinkan seluruh blok tetap utuh saat bergerak.</p>
+            </div>
 
-            <h4>Aliran Bahan Rombakan</h4>
             <div class="image">
                 <img src="<?= img ?>img/aliranbahanrombakan.png" alt="Aliran Rombakan" class="image">
             </div>
-            <p>Longsor ini terjadi karera massa tanah bergerak
-                didorong oleh air. Biasanya terjadi di sepanjang
-                lembah dan mampu mencapai ratusan meter. Kecepatan
-                longsoran ini dipengaruhi oleh kemiringan tanah
-                itu sendiri</p>
+            <div class="content">
+                <h4>Aliran Bahan Rombakan</h4>
+                <p>Longsor ini terjadi karera massa tanah bergerak
+                    didorong oleh air. Biasanya terjadi di sepanjang
+                    lembah dan mampu mencapai ratusan meter. Kecepatan
+                    longsoran ini dipengaruhi oleh kemiringan tanah
+                    itu sendiri</p>
+            </div>
         </div>
 
         <div class="Gejala" id="scrollspyGejala">
@@ -462,11 +563,14 @@
     <div class="data" data-bs-spy="scroll" data-bs-target="#navspymenu">
         <div class="mitigasi" id="scrollspyMitigasi">
             <h2>Mitigasi</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur
-                adipisicing elit. Ratione iusto aliquid laudantium.
-                Quis dolore quaerat tenetur dolores earum tempora
-                incidunt, sed optio sunt sint, aspernatur fugit.
-                Nobis quis quod sint.</p>
+            <p>Mitigasi bencana adalah upaya atau tindakan yang dilakukan
+                untuk mengurangi atau meminimalkan dampak negatif dari
+                bencana alam atau bencana buatan manusia. Tujuannya adalah
+                untuk mengurangi risiko, kerugian, dan kerusakan yang mungkin
+                terjadi akibat bencana. Mitigasi bencana melibatkan
+                langkah-langkah pencegahan, persiapan, dan perlindungan
+                agar masyarakat, infrastruktur, dan lingkungan dapat lebih
+                siap menghadapi bencana dan meminimalkan dampaknya.</p>
             <p>Lorem ipsum dolor, sit amet consectetur
                 adipisicing elit. Ratione iusto aliquid laudantium.
                 Quis dolore quaerat tenetur dolores earum tempora
